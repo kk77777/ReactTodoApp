6ea57744786b3bcaf8fdf51eb0d4805d965cd271
kk77777 <kaushikgattani@gmail.com>
2020-02-19 15:52:24 +0530

    Changed functional component to stateful class component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 
 import TodoItem from './Components/TodoItem';
 import todosData from './TodosData';
 
-function App() {
-  const todoComponents = todosData.map(item => <TodoItem item={item} />);
-  return <div className='todo-list'>{todoComponents}</div>;
+// function App() {
+//   const todoComponents = todosData.map(item => <TodoItem item={item} />);
+// return <div className='todo-list'>{todoComponents}</div>;
+// }
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      todos: todosData
+    };
+  }
+
+  render() {
+    const todoComponents = this.state.todos.map(item => (
+      <TodoItem item={item} />
+    ));
+    return <div className='todo-list'>{todoComponents}</div>;
+  }
 }
 
 export default App;
