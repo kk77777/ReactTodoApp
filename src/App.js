0228b2dcce307e5070795836e7a71b733dad4be2
kk77777 <kaushikgattani@gmail.com>
2020-02-19 16:26:28 +0530

    Removed warnings

--- src/App.js
+++ src/App.js
@@ -4,11 +4,6 @@ import './App.css';
 import TodoItem from './Components/TodoItem';
 import todosData from './TodosData';
 
-// function App() {
-//   const todoComponents = todosData.map(item => <TodoItem item={item} />);
-// return <div className='todo-list'>{todoComponents}</div>;
-// }
-
 class App extends Component {
   constructor() {
     super();
@@ -19,7 +14,7 @@ class App extends Component {
 
   render() {
     const todoComponents = this.state.todos.map(item => (
-      <TodoItem item={item} />
+      <TodoItem key={item.id} item={item} />
     ));
     return <div className='todo-list'>{todoComponents}</div>;
   }
