0c9677cf6abf5aeb9631bc4848ac27381b386194
kk77777 <kaushikgattani@gmail.com>
2020-02-20 11:55:26 +0530

    added functionality

--- src/App.js
+++ src/App.js
@@ -10,11 +10,26 @@ class App extends Component {
     this.state = {
       todos: todosData
     };
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(id) {
+    this.setState(prevState => {
+      const updateTodos = prevState.todos.map(todo => {
+        if (todo.id === id) {
+          todo.completed = !todo.completed;
+        }
+        return todo;
+      });
+      return {
+        todos: updateTodos
+      };
+    });
   }
 
   render() {
     const todoComponents = this.state.todos.map(item => (
-      <TodoItem key={item.id} item={item} />
+      <TodoItem key={item.id} item={item} handleChange={this.handleChange} />
     ));
     return <div className='todo-list'>{todoComponents}</div>;
   }
